73a26bfa9dced61b5ce0a4f02dd7b9c03542dc8d
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2023-12-06 10:25:47 -0300

    feat: primeiro problema
    Nesse primeiro problema temos uma classe que executa uma multiplicação de maneira muito ineficiente, demorando alguns segundos para calcular valores grandes, enquanto isso o sistema fica travado (tela congelada).

---
+++ studying-threads/src/com/github/pablwoAraujo/Main.java
@@ -0,0 +1,12 @@
+package com.github.pablwoAraujo;
+
+public class Main {
+	public static void main(String[] args) throws InterruptedException {
+
+		System.out.println("Main thread");
+		
+		Thread.sleep(5000);
+		
+		System.out.println("Main thread");
+	}
+}
