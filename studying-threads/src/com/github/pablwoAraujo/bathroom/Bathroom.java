08e7e4fa7f713e51d47f6724e5c201e3ad15f59e
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2023-12-06 18:26:52 -0300

    feat: usando ReentrantLock
    Outra forma de sincronizar o acesso a tarefa desejada, esse método bloqueia a região do código de forma mais explícita

--- studying-threads/src/com/github/pablwoAraujo/bathroom/Bathroom.java
+++ studying-threads/src/com/github/pablwoAraujo/bathroom/Bathroom.java
@@ -1,33 +1,42 @@
 package com.github.pablwoAraujo.bathroom;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Bathroom {
+
+	private Lock lock = new ReentrantLock();
 
 	public void makeNumberOne() {
 		String name = Thread.currentThread().getName();
 		System.out.println(name + " Batendo na porta");
 
-		synchronized (this) {
-			System.out.println(name + " Entrando no banheiro");
-			System.out.println(name + " Fazendo coisa rápida");
-			threadSleep(5000);
-			System.out.println(name + " Dando descarga");
-			System.out.println(name + " Lavando as mãos");
-			System.out.println(name + " Saindo no banheiro");
-		}
+		// Bloqueia o acesso ao "banheiro"
+		lock.lock();
+		System.out.println(name + " Entrando no banheiro");
+		System.out.println(name + " Fazendo coisa rápida");
+		threadSleep(5000);
+		System.out.println(name + " Dando descarga");
+		System.out.println(name + " Lavando as mãos");
+		System.out.println(name + " Saindo no banheiro");
+		// Libera o acesso
+		lock.unlock();
 	}
 
 	public void makeNumberTwo() {
 		String name = Thread.currentThread().getName();
 		System.out.println(name + " Batendo na porta");
 
-		synchronized (this) {
-			System.out.println(name + " Entrando no banheiro");
-			System.out.println(name + " Fazendo coisa demorada");
-			threadSleep(10000);
-			System.out.println(name + " Dando descarga");
-			System.out.println(name + " Lavando as mãos");
-			System.out.println(name + " Saindo no banheiro");
-		}
+		// Bloqueia o acesso ao "banheiro"
+		lock.lock();
+		System.out.println(name + " Entrando no banheiro");
+		System.out.println(name + " Fazendo coisa demorada");
+		threadSleep(10000);
+		System.out.println(name + " Dando descarga");
+		System.out.println(name + " Lavando as mãos");
+		System.out.println(name + " Saindo no banheiro");
+		// Libera o acesso
+		lock.unlock();
 	}
 
 	private void threadSleep(int time) {
