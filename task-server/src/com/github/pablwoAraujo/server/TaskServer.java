765c08a1601dd6e715916caef45300b54b916a5c
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2023-12-22 11:08:03 -0300

    feat: usando AtomicBoolean
    Para evitar que cada thread faça o cache desse atributo e, quando houver uma alteração, ela seja refletida diretamente na memória, utilizamos o tipo AtomicBoolean

--- task-server/src/com/github/pablwoAraujo/server/TaskServer.java
+++ task-server/src/com/github/pablwoAraujo/server/TaskServer.java
@@ -6,19 +6,26 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class TaskServer {
 
 	private static final int port = 12345;
 	private ServerSocket server;
 	private ExecutorService threadPool;
-	private boolean running;
+	// Para evitar que cada thread faça o cache desse atributo e, quando houver
+	// uma alteração, ela seja refletida diretamente na memória, utilizamos o
+	// modificador "volatile"
+	// private volatile boolean running;
+	// Outra alternativa é usar o tipo "AtomicBoolean", que implementa por baixo
+	// dos panos um boolean com o modificador "volatile".
+	private AtomicBoolean running;
 
 	public TaskServer() throws IOException {
 		System.out.println("--- Starting the server ---");
 		this.server = new ServerSocket(port);
 		this.threadPool = Executors.newCachedThreadPool();
-		this.running = true;
+		this.running = new AtomicBoolean(true);
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -28,13 +35,14 @@ public class TaskServer {
 	}
 
 	private void run() throws IOException {
-		while (this.running) {
+		while (this.running.get()) {
 			try {
 				Socket socket = server.accept();
 				System.out.println("Aceitando novo client na porta: " + socket.getPort());
 
 				AllocateTasks allocateTasks = new AllocateTasks(socket, this);
-				threadPool.execute(allocateTasks); // Buscando uma thread da pool para executar a tarefa
+				// Buscando uma thread da pool para executar a tarefa
+				threadPool.execute(allocateTasks);
 			} catch (SocketException e) {
 				System.out.println(e);
 			}
@@ -42,7 +50,7 @@ public class TaskServer {
 	}
 
 	public void stop() throws IOException {
-		this.running = false;
+		this.running.set(false);
 		server.close();
 		threadPool.shutdown();
 	}
